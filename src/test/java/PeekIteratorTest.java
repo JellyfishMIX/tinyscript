70a7c3ba33459f0154a35b1865b64da4d0acb731
JellyfishMIX <2273516339@qq.com>
2020-05-05 02:33:07 +0800

    :white_check_mark: Added the PeekIteratorTest

--- src/test/java/PeekIteratorTest.java
+++ src/test/java/PeekIteratorTest.java
@@ -1,5 +1,8 @@
 import common.PeekIterator;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author JellyfishMIX
@@ -7,7 +10,47 @@ import org.junit.jupiter.api.Test;
  */
 public class PeekIteratorTest {
     @Test
+    @Disabled
     public void test_peek() {
+        var source = "abcdefg";
+        var it = new PeekIterator<Character>(source.chars().mapToObj(c -> (char) c));
 
+        assertEquals('a', it.next());
+        assertEquals('b', it.next());
+        it.next();
+        it.next();
+        assertEquals('e', it.next());
+        assertEquals('f', it.peek());
+        assertEquals('f', it.peek());
+        assertEquals('f', it.next());
+        assertEquals('g', it.next());
+    }
+
+    @Test
+    @Disabled
+    public void test_lookahead2() {
+        var source = "abcdefg";
+        var it = new PeekIterator<Character>(source.chars().mapToObj(c -> (char) c));
+        assertEquals('a', it.next());
+        assertEquals('b', it.next());
+        assertEquals('c', it.next());
+        it.putBack();
+        it.putBack();
+        assertEquals('b', it.next());
+    }
+
+    @Test
+    @Disabled
+    public void test_endToken() {
+        var source = "abcdefg";
+        var it = new PeekIterator<Character>(source.chars().mapToObj(c -> (char)c), (char)0);
+        var i = 0;
+        while (it.hasNext()) {
+            if (i == 7) {
+                assertEquals((char)0, it.next());
+            } else {
+                assertEquals(source.charAt(i++), it.next());
+            }
+        }
     }
 }
