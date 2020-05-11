534362e9a6cb1fe99d05b3e5c587f47cd71ee886
JellyfishMIX <2273516339@qq.com>
2020-05-12 00:01:25 +0800

    :white_check_mark: The test of "SimpleParser" has passed

--- src/test/java/parser/SimpleParserTest.java
+++ src/test/java/parser/SimpleParserTest.java
@@ -19,33 +19,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class SimpleParserTest {
 
     @Test
-    // @Disabled
+    @Disabled
     void parse() throws LexicalException, ParseException {
         Stream<Character> source = "1+2+3+4".chars().mapToObj(x -> (char)x);
         Lexer lexer = new Lexer();
         PeekTokenIterator it = new PeekTokenIterator(lexer.analyse(source).stream());
         ASTNode expr = SimpleParser.parse(it);
 
-        // assertEquals(2, expr.getChildren().size());
-        // ASTNode c1 = expr.getChild(0);
-        // assertEquals("1", c1.getLexeme().getValue());
-        // assertEquals("+", expr.getLexeme().getValue());
-        //
-        // ASTNode c2 = expr.getChild(1);
-        // ASTNode d1 = c2.getChild(0);
-        // assertEquals("2", d1.getLexeme().getValue());
-        // assertEquals("+", c2.getLexeme().getValue());
-        //
-        // ASTNode d2 = expr.getChild(1);
-        // // ASTNode e1 = d2.getChild(0);
-        // // assertEquals("3", e1.getLexeme().getValue());
-        // // assertEquals("+", d2.getLexeme().getValue());
-        //
-        // ASTNode e2 = d2.getChild(1);
-        // ASTNode e1 = d2.getChild(0);
-        // System.out.println(e1.getLexeme().getValue());
-        //
-        // // assertEquals("4", e2.getLexeme().getValue());
+        assertEquals(2, expr.getChildren().size());
+        ASTNode c1 = expr.getChild(0);
+        assertEquals("1", c1.getLexeme().getValue());
+        assertEquals("+", expr.getLexeme().getValue());
+
+        ASTNode c2 = expr.getChild(1);
+        ASTNode d1 = c2.getChild(0);
+        assertEquals("2", d1.getLexeme().getValue());
+        assertEquals("+", c2.getLexeme().getValue());
+
+        ASTNode d2 = c2.getChild(1);
+        ASTNode e1 = d2.getChild(0);
+        assertEquals("3", e1.getLexeme().getValue());
+        assertEquals("+", d2.getLexeme().getValue());
+
+        ASTNode e2 = d2.getChild(1);
+        assertEquals("4", e2.getLexeme().getValue());
+
         expr.print(0);
     }
 }
